a79d4077a34595458c08dd3bc7f0151aaf4daf3d
samiksha12 <sapkota.samiksha@gmail.com>
2022-06-04 14:05:27 -0500

    create grouptitle.js

---
+++ grouptitle.js
@@ -0,0 +1,18 @@
+let result = [];
+let terms = [
+  "duel",
+  "dule",
+  "speed",
+  "spede",
+  "deul",
+  "cars",
+  "racs",
+  "shape",
+  "shpea",
+  "tiger",
+  "triger"
+];
+function groupTitle() {
+    let userInput = prompt("Enter the word");
+    console.log("work in progress");
+}
